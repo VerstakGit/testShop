65d67720d081ec39e9f360effdfea3c1dd5e97e5
Swetoslaw2142 <swetoslaw2142@mail.ru>
2021-03-13 15:17:53 +0300

    Tests created.

--- main.py
+++ main.py
@@ -2,18 +2,24 @@ from aiohttp import web
 from handlers import couriers, update_couriers, orders, assign_orders, complete_orders, courier_info
 import sqlite3
 
-app = web.Application()
-try:
-    db = sqlite3.connect("database.db")
-    app['db'] = db
-except sqlite3.Error as e:
-    print(e)
-app.add_routes([web.post('/couriers', couriers),
-                web.patch('/couriers/{id}', update_couriers),
-                web.post('/orders', orders),
-                web.post('/orders/assign', assign_orders),
-                web.post('/orders/complete', complete_orders),
-                web.get('/couriers/{id}', courier_info)])
+
+def create_application(path):
+    app = web.Application()
+    try:
+        db = sqlite3.connect(path)
+        app['db'] = db
+    except sqlite3.Error as e:
+        print(e)
+    app.add_routes([web.post('/couriers', couriers),
+                    web.patch('/couriers/{id}', update_couriers),
+                    web.post('/orders', orders),
+                    web.post('/orders/assign', assign_orders),
+                    web.post('/orders/complete', complete_orders),
+                    web.get('/couriers/{id}', courier_info)])
+    return app
+
+
+app = create_application("database.db")
 
 if __name__ == '__main__':
     web.run_app(app)
---
+++ test_handlers.py
@@ -0,0 +1,300 @@
+from aiohttp import web
+from handlers import couriers
+from main import create_application
+
+from courier import Courier
+
+
+def reset_db(db):
+    cur = db.cursor()
+    cur.execute('DELETE FROM couriers')
+    cur.execute('DELETE FROM orders')
+    db.commit()
+
+
+async def test_courier(aiohttp_client, loop):
+    app = create_application("test_database.db")
+    reset_db(app['db'])
+    client = await aiohttp_client(app)
+
+    resp = await client.post('/couriers', json={"empty": "fake"})
+    assert resp.status == 400
+    text = await resp.text()
+    assert text == ''
+
+    tests = [
+        {
+            "body": {
+                "data": [{"courier_id": 1, "courier_type": "foot", "regions": [1], "working_hours": ["11:00-18:00"]},
+                         {"courier_id": 2, "type": "unknown"}]},
+            "status": 400,
+            "result": {"validation_error": {"couriers": [{"id": 2}]}}
+        },
+        {
+            "body": {
+                "data": [{"courier_id": 1, "courier_type": "unknown", "regions": [1], "working_hours": ["11:00-18:00"]},
+                         {"courier_id": 2, "regions": "2", "courier_type": "bike", "working_hours": ["9:00-10:00"]}]},
+            "status": 400,
+            "result": {"validation_error": {"couriers": [{"id": 1}, {"id": 2}]}},
+        },
+        {
+            "body": {
+                "data": [{"courier_id": 1, "courier_type": "foot", "regions": [1], "working_hours": ["11:00-18:00"]},
+                         {"courier_id": 2, "courier_type": "car", "regions": [1, 2, 3, 4],
+                          "working_hours": ["9:00-15:00", "18:00-21:00"]}]},
+            "status": 201,
+            "result": {"couriers": [{"id": 1}, {"id": 2}]}
+        }
+    ]
+
+    for test in tests:
+        resp = await client.post('/couriers', json=test["body"])
+        assert resp.status == test["status"]
+        json = await resp.json()
+        assert json == test["result"]
+
+
+async def test_update_couriers(aiohttp_client, loop):
+    app = create_application("test_database.db")
+    reset_db(app['db'])
+    client = await aiohttp_client(app)
+    tests = [
+        {
+            "create_body": {
+                "data": [{"courier_id": 1, "courier_type": "foot", "regions": [1], "working_hours": ["11:00-18:00"]}]},
+            "create_status": 201,
+            "courier_id": 1,
+            "update_body": {"regions": [1, 2, 3, 4]},
+            "update_status": 200,
+            "result": {"courier_id": 1, "courier_type": "foot", "regions": [1, 2, 3, 4],
+                       "working_hours": ["11:00-18:00"]}
+        },
+        {
+            "create_body": {
+                "data": [{"courier_id": 1, "courier_type": "foot", "regions": [1], "working_hours": ["11:00-18:00"]}]},
+            "create_status": 201,
+            "courier_id": 1,
+            "update_body": {"regions": "f"},
+            "update_status": 400,
+        },
+        {
+            "create_body": {
+                "data": [{"courier_id": 1, "courier_type": "foot", "regions": [1], "working_hours": ["11:00-18:00"]}]},
+            "create_status": 201,
+            "courier_id": 1,
+            "update_body": {"courier_type": "bike", "working_hours": ["9:00-21:00"]},
+            "update_status": 200,
+            "result": {"courier_id": 1, "courier_type": "bike", "regions": [1],
+                       "working_hours": ["9:00-21:00"]}
+        },
+    ]
+
+    for test in tests:
+        resp = await client.post('/couriers', json=test["create_body"])
+        assert resp.status == test["create_status"]
+        resp = await client.patch(f'/couriers/{test["courier_id"]}', json=test["update_body"])
+        assert resp.status == test["update_status"]
+        if test["update_status"] == 200:
+            json = await resp.json()
+            assert json == test["result"]
+
+
+async def test_orders(aiohttp_client, loop):
+    app = create_application("test_database.db")
+    reset_db(app['db'])
+    client = await aiohttp_client(app)
+    tests = [
+        {
+            "body": {"data": [{"order_id": 1, "weight": 0.23, "region": 10, "delivery_hours": ["09:00-12:00"]},
+                              {"order_id": 2, "weight": 10, "region": 43,
+                               "delivery_hours": ["12:00-15:00", "18:00-23:00"]}]},
+            "status": 201,
+            "result": {"orders": [{"id": 1}, {"id": 2}]},
+        },
+        {
+            "body": {"data": [{"order_id": 1, "weight": 0.009, "region": 10, "delivery_hours": ["09:00-12:00"]},
+                              {"order_id": 2, "weight": 50.1, "region": 43,
+                               "delivery_hours": ["12:00-15:00", "18:00-23:00"]},
+                              {"order_id": 3, "weight": 1, "region": -1, "delivery_hours": ["09:00-12:00"]},
+                              {"order_id": 4, "weight": 2, "region": 12, "delivery_hours": [1, 2, 3]}]},
+            "status": 400,
+            "result": {"validation_error": {"orders": [{"id": 1}, {"id": 2}, {"id": 3}, {"id": 4}]}},
+        }
+    ]
+
+    for test in tests:
+        resp = await client.post('/orders', json=test["body"])
+        assert resp.status == test["status"]
+        json = await resp.json()
+        assert json == test["result"]
+
+
+async def test_orders_assign(aiohttp_client, loop):
+    app = create_application("test_database.db")
+    client = await aiohttp_client(app)
+    tests = [
+        {
+            "courier_body": {
+                "data": [{"courier_id": 1, "courier_type": "foot", "regions": [1], "working_hours": ["11:00-18:00"]}]},
+            "order_body": {"data": [{"order_id": 1, "weight": 0.23, "region": 1, "delivery_hours": ["09:00-12:00"]},
+                                    {"order_id": 2, "weight": 10, "region": 2,
+                                     "delivery_hours": ["12:00-15:00", "18:00-23:00"]}]},
+            "assign_body": {"courier_id": 1},
+            "final_status": 200,
+            "final_resp": {"orders": [{"id": 1}]},
+        },
+        {
+            "courier_body": {
+                "data": [{"courier_id": 1, "courier_type": "foot", "regions": [1], "working_hours": ["11:00-18:00"]}]},
+            "order_body": {"data": [{"order_id": 1, "weight": 0.23, "region": 1, "delivery_hours": ["09:00-12:00"]},
+                                    {"order_id": 2, "weight": 10, "region": 2,
+                                     "delivery_hours": ["12:00-15:00", "18:00-23:00"]}]},
+            "assign_body": {"courier_id": 3},
+            "final_status": 400,
+            "final_resp": {"orders": [{"id": 1}]},
+        },
+        {
+            "courier_body": {
+                "data": [{"courier_id": 1, "courier_type": "foot", "regions": [1], "working_hours": ["11:00-18:00"]}]},
+            "order_body": {"data": [{"order_id": 1, "weight": 0.23, "region": 1, "delivery_hours": ["09:00-12:00"]},
+                                    {"order_id": 2, "weight": 0.1, "region": 2,
+                                     "delivery_hours": ["12:00-15:00", "18:00-23:00"]},
+                                    {"order_id": 3, "weight": 9, "region": 1,
+                                     "delivery_hours": ["15:00-21:00", "23:00-23:59"]},
+                                    {"order_id": 4, "weight": 0.2, "region": 1,
+                                     "delivery_hours": ["09:00-10:30", "19:00-23:00"]},
+                                    {"order_id": 5, "weight": 1, "region": 1, "delivery_hours": ["07:00-14:00"]}]},
+            "assign_body": {"courier_id": 1},
+            "final_status": 200,
+            "final_resp": {"orders": [{"id": 1}, {"id": 3}]},
+        }
+    ]
+
+    for test in tests:
+        reset_db(app['db'])
+        resp = await client.post('/couriers', json=test["courier_body"])
+        assert resp.status == 201
+        resp = await client.post('/orders', json=test['order_body'])
+        assert resp.status == 201
+        resp = await client.post('/orders/assign', json=test['assign_body'])
+        assert resp.status == test['final_status']
+        if test['final_status'] == 200:
+            json = await resp.json()
+            assert json == test['final_resp']
+
+
+async def test_orders_complete(aiohttp_client, loop):
+    app = create_application("test_database.db")
+    client = await aiohttp_client(app)
+    tests = [
+        {
+            "courier_body": {
+                "data": [{"courier_id": 1, "courier_type": "foot", "regions": [1], "working_hours": ["11:00-18:00"]}]},
+            "order_body": {"data": [{"order_id": 1, "weight": 0.23, "region": 1, "delivery_hours": ["09:00-12:00"]},
+                                    {"order_id": 2, "weight": 10, "region": 2,
+                                     "delivery_hours": ["12:00-15:00", "18:00-23:00"]}]},
+            "assign_body": {"courier_id": 1},
+            "complete_bodies": [{"courier_id": 1, "order_id": 1}, {"courier_id": 1, "order_id": 1},
+                                {"courier_id": 1, "order_id": 2}, {"courier_id": 2, "order_id": 1}],
+            "final_statuses": [200, 200, 400, 400],
+            "final_resps": [{"order_id": 1}, {"order_id": 1}, {}, {}],
+        },
+        {
+            "courier_body": {
+                "data": [{"courier_id": 1, "courier_type": "foot", "regions": [1], "working_hours": ["11:00-18:00"]},
+                         {"courier_id": 2, "courier_type": "car", "regions": [2, 3],
+                          "working_hours": ["09:00-18:00"]}]},
+            "order_body": {"data": [{"order_id": 1, "weight": 0.23, "region": 1, "delivery_hours": ["09:00-12:00"]},
+                                    {"order_id": 2, "weight": 0.1, "region": 2,
+                                     "delivery_hours": ["12:00-15:00", "18:00-23:00"]},
+                                    {"order_id": 3, "weight": 9, "region": 1,
+                                     "delivery_hours": ["15:00-21:00", "23:00-23:59"]},
+                                    {"order_id": 4, "weight": 0.2, "region": 1,
+                                     "delivery_hours": ["09:00-10:30", "19:00-23:00"]},
+                                    {"order_id": 5, "weight": 1, "region": 1, "delivery_hours": ["07:00-14:00"]}]},
+            "assign_body": {"courier_id": 1},
+            "complete_bodies": [{"courier_id": 1, "order_id": 1}, {"courier_id": 1, "order_id": 2},
+                                {"courier_id": 1, "order_id": 3}, {"courier_id": 1, "order_id": 4},
+                                {"courier_id": 1, "order_id": 5}, {"courier_id": 2, "order_id": 1},
+                                {"courier_id": 2, "order_id": 2}],
+            "final_statuses": [200, 400, 200, 400, 400, 400, 400],
+            "final_resps": [{"order_id": 1}, {}, {"order_id": 3}, {}, {}, {}, {}],
+        }
+    ]
+
+    for test in tests:
+        reset_db(app['db'])
+        resp = await client.post('/couriers', json=test["courier_body"])
+        assert resp.status == 201
+        resp = await client.post('/orders', json=test['order_body'])
+        assert resp.status == 201
+        resp = await client.post('/orders/assign', json=test['assign_body'])
+        assert resp.status == 200
+        for i in range(len(test['complete_bodies'])):
+            resp = await client.post('/orders/complete', json=test['complete_bodies'][i])
+            assert resp.status == test['final_statuses'][i]
+            if test['final_statuses'][i] == 200:
+                json = await resp.json()
+                assert json == test['final_resps'][i]
+
+
+async def test_courier_info(aiohttp_client, loop):
+    app = create_application("test_database.db")
+    client = await aiohttp_client(app)
+    tests = [
+        {
+            "courier_body": {
+                "data": [{"courier_id": 1, "courier_type": "foot", "regions": [1], "working_hours": ["11:00-18:00"]}]},
+            "order_body": {"data": [{"order_id": 1, "weight": 0.23, "region": 1, "delivery_hours": ["09:00-12:00"]},
+                                    {"order_id": 2, "weight": 10, "region": 2,
+                                     "delivery_hours": ["12:00-15:00", "18:00-23:00"]}]},
+            "assign_body": {"courier_id": 1},
+            "complete_bodies": [{"courier_id": 1, "order_id": 1}],
+            "courier_ids": [1, 2],
+            "final_statuses": [200, 400],
+            "final_resps": [
+                {"courier_id": 1, "courier_type": "foot", "regions": [1], "working_hours": ["11:00-18:00"], "rating": 5,
+                 "earnings": 1000}, {}],
+        },
+        {
+            "courier_body": {
+                "data": [{"courier_id": 1, "courier_type": "bike", "regions": [1], "working_hours": ["11:00-18:00"]},
+                         {"courier_id": 2, "courier_type": "car", "regions": [2, 3],
+                          "working_hours": ["09:00-18:00"]}]},
+            "order_body": {"data": [{"order_id": 1, "weight": 0.23, "region": 1, "delivery_hours": ["09:00-12:00"]},
+                                    {"order_id": 2, "weight": 0.1, "region": 2,
+                                     "delivery_hours": ["12:00-15:00", "18:00-23:00"]},
+                                    {"order_id": 3, "weight": 9, "region": 1,
+                                     "delivery_hours": ["15:00-21:00", "23:00-23:59"]},
+                                    {"order_id": 4, "weight": 0.2, "region": 1,
+                                     "delivery_hours": ["09:00-10:30", "19:00-23:00"]},
+                                    {"order_id": 5, "weight": 1, "region": 1, "delivery_hours": ["07:00-14:00"]}]},
+            "assign_body": {"courier_id": 1},
+            "complete_bodies": [{"courier_id": 1, "order_id": 1}, {"courier_id": 1, "order_id": 3}],
+            "courier_ids": [1, 2],
+            "final_statuses": [200, 200],
+            "final_resps": [
+                {"courier_id": 1, "courier_type": "bike", "regions": [1], "working_hours": ["11:00-18:00"], "rating": 5,
+                 "earnings": 2500},
+                {"courier_id": 2, "courier_type": "car", "regions": [2, 3], "working_hours": ["09:00-18:00"],
+                 "rating": 0, "earnings": 0}],
+        }
+    ]
+
+    for test in tests:
+        reset_db(app['db'])
+        resp = await client.post('/couriers', json=test["courier_body"])
+        assert resp.status == 201
+        resp = await client.post('/orders', json=test['order_body'])
+        assert resp.status == 201
+        resp = await client.post('/orders/assign', json=test['assign_body'])
+        assert resp.status == 200
+        for i in range(len(test['complete_bodies'])):
+            resp = await client.post('/orders/complete', json=test['complete_bodies'][i])
+            assert resp.status == 200
+        for i in range(len(test['courier_ids'])):
+            resp = await client.get(f'/couriers/{test["courier_ids"][i]}')
+            assert resp.status == test["final_statuses"][i]
+            if test["final_statuses"] == 200:
+                json = await resp.json()
+                assert json == test["final_resps"][i]
